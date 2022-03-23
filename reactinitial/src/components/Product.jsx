c01a1284ca227ce44677d2f9b4efade6f12ec28d
GegenyPeter <gegeny.p@gmail.com>
2022-03-23 15:31:57 +0100

    done

--- reactinitial/src/components/Product.jsx
+++ reactinitial/src/components/Product.jsx
@@ -4,13 +4,16 @@ import SubProduct from './SubProduct'
 
 function Product(props) {
     const [isShown, setIsShown] = useState(false)
+    const [filterWord, setFilterWord] = useState("")
     const {cloth} = props
   return (
     <div>
         <h2>{cloth.type}</h2>
+        <input value={filterWord} type="text" onChange={(e) => setFilterWord(e.target.value)}></input>
         <h3>{cloth.gender}</h3>
         {isShown ? cloth.products.map((p, index) => <SubProduct key={index} p={p} />) : <></>}
         <button onClick={()=>setIsShown(!isShown)}>{isShown ? "Show less" : "Show More"}</button>
+        <hr></hr>
     </div>
   )
 }
