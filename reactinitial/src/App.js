0bbdb3fff462c5e448d81b73238ed08bcf9a4c97
GegenyPeter <gegeny.p@gmail.com>
2022-03-23 15:12:25 +0100

    api call

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 const App = () => {
+const [clothes, setClothes] = useState([])
+
+  const load = async () => {
+    const response = await axios('https://demoapi.com/api/api/clothes')
+    setClothes(await response.data)
+  }
+
+  useEffect(() => {
+    load()
+  }, [])
 
   return (
     <div>
-      Todo...
+      <h1>Webshop</h1>
     </div>
   )
 }
